29178d821ecef55895b25658ca1f44506aa24d47
Victor <ban.victor728@gmail.com>
2024-07-17 09:33:17 -0400

    worked through the featured puppy tab

--- src/App.jsx
+++ src/App.jsx
@@ -3,17 +3,31 @@ import { useState } from 'react'
 
 const App = () => {
  const [puppies, setPuppies] = useState(puppyList) 
+ const [featPupId, setFeatPupId] = useState(null)
 
-
+ const featuredPup = puppies.find((pup)=> pup.id === featPupId)
+  console.log(featuredPup)
 
 
   return (
     <div>
+      <h1> The Puppy List </h1>
+
        {puppies.map((puppy) => {
-        return <p key={puppy.id}> {puppy.name} </p>
+        return <p onClick={()=>{setFeatPupId(puppy.id)}} key={puppy.id}>
+          {puppy.name}
+        </p>
     })}
 
-
+      {featPupId && (
+        <div>
+          <h2>{featuredPup.name}</h2>
+          <ul>
+            <li>Age: {featuredPup.age}</li>
+            <li>Email: {featuredPup.email}</li>
+          </ul>
+        </div>
+      )}
 
     </div>
   )
